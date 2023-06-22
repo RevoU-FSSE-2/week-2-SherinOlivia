012efcc2e20f0a6e044b5e2ffb4d099b74cf8495
Sherin Olivia <SOChronicle@gmail.com>
2023-06-23 06:44:59 +0700

    added js functions to fix

---
+++ script.js
@@ -0,0 +1,36 @@
+let slideshowPhotos = 1;
+showSlideshow(slideshowPhotos);
+
+// next 
+function nextPhoto(n) {
+    showSlideshow(slideshowPhotos += n);
+}
+
+// dots click click
+function currentSlide(n) {
+    showSlideshow(slideshowPhotos = n);
+}
+
+// function showSlideshow(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("slideshowGallery");
+//     let dots = document.getElementsByClassName("dot");
+
+//     if (n > slides.length) {
+//         slideshowPhotos = 1
+//     }
+
+//     if (n < 1) {
+//         slideshowPhotos = slides.length
+//     }
+
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//       }
+
+//       for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//       }
+//       slides[slideIndex-1].style.display = "block";
+//       dots[slideIndex-1].className += " active";
+// }
