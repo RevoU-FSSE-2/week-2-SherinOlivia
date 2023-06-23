a43c9cbd7e5bb8d83441decf2ac6c219792bd258
Sherin Olivia <SOChronicle@gmail.com>
2023-06-23 23:48:28 +0700

    fixed form and added hover to grid pics

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-let slideshowPhotos = 1;
-showSlideshow(slideshowPhotos);
+function whySubmit() {
+    alert("Can't be bothered with all that.. Download and play the game. HMU after")
+}
+
+
+// let slideshowPhotos = 1;
+// showSlideshow(slideshowPhotos);
 
 // next 
-function nextPhoto(n) {
-    showSlideshow(slideshowPhotos += n);
-}
+// function nextPhoto(n) {
+//     showSlideshow(slideshowPhotos += n);
+// }
 
 // dots click click
-function currentSlide(n) {
-    showSlideshow(slideshowPhotos = n);
-}
+// function currentSlide(n) {
+//     showSlideshow(slideshowPhotos = n);
+// }
 
 // function showSlideshow(n) {
 //     let i;
