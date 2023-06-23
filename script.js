1737b7d494d07b513234df0cc3570ff4e8ae5a64
Sherin Olivia <SOChronicle@gmail.com>
2023-06-24 03:41:39 +0700

    90% finished without hamburger button and only ReadME.md file left

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
+// // HamburgerMenu
+// function hamburgerMenu() {
+//     var x = document.getElementsByClassName("hamburger");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+
+//   Slideshow
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+function nextPhoto(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slideshow-gallery");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// Form 
 function whySubmit() {
     alert("Can't be bothered with all that.. Download and play the game. HMU after")
 }
-
-
-// let slideshowPhotos = 1;
-// showSlideshow(slideshowPhotos);
-
-// next 
-// function nextPhoto(n) {
-//     showSlideshow(slideshowPhotos += n);
-// }
-
-// dots click click
-// function currentSlide(n) {
-//     showSlideshow(slideshowPhotos = n);
-// }
-
-// function showSlideshow(n) {
-//     let i;
-//     let slides = document.getElementsByClassName("slideshowGallery");
-//     let dots = document.getElementsByClassName("dot");
-
-//     if (n > slides.length) {
-//         slideshowPhotos = 1
-//     }
-
-//     if (n < 1) {
-//         slideshowPhotos = slides.length
-//     }
-
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//       }
-
-//       for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//       }
-//       slides[slideIndex-1].style.display = "block";
-//       dots[slideIndex-1].className += " active";
-// }
